a88a2a0d695499537e56a3b7f035ecfca2612523
Shahed-Tareq <ahmadjehad147@gmail.com>
2022-11-01 09:54:47 +0200

    update database

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,6 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'username' => fake()->unique()->userName(),
             'firstname' => fake()->firstName(),
